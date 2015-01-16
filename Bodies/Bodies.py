59c8dc66652f951266f4eb3913dd5f12b790c08c
Washu2199 <shintaro2199@gmail.cmo>
2015-01-16 16:06:19 +0900

    変更

--- Bodies/Bodies.py
+++ Bodies/Bodies.py
@@ -3,7 +3,7 @@
 
 import sys
 
-from jp.ac.kyoto_su.cse.TSKS.mvc.Example import Example as example
+from jp.ac.kyoto_su.cse.TSKS.Example import Example as example
 
 if __name__ == '__main__':
 	sys.exit(example.main())
